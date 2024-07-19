60aa242458418bb13b8a6a0dc057f95735d18b4a
Inez Małecka <maleckainez@gmail.com>
2024-07-19 23:20:33 +0300

    Dodano mase szitu

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('scroll', function() {
         const navbar = document.querySelector('.navbar');
+        const gora = document.querySelector('.dogory');
         if (window.scrollY >= 5) {
             navbar.classList.add("scrollowany");
+            gora.classList.add("scrollowany");
         } else {
             navbar.classList.remove("scrollowany");
+            gora.classList.remove("scrollowany");
         }
         console.log('Current classes on navbar:', navbar.className); // Dodaj ten wiersz
     });
@@ -18,4 +21,20 @@ document.addEventListener('DOMContentLoaded', function() {
         destination.scrollIntoView({behavior: 'smooth'});
     }})
 })
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.navbar .links a');
+
+    function changeLinkState() {
+        let index = sections.length;
+
+        while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
+
+        navLinks.forEach((link) => link.classList.remove('active'));
+        navLinks[index].classList.add('active');
+    }
+
+    changeLinkState();
+    window.addEventListener('scroll', changeLinkState);
+});
 
